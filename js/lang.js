0933560fcf22c8532a0fff32386b0748a68c4b23
uo44993 <uo44993@uniovi.es>
2025-12-13 18:12:51 +0100

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -1,3 +1,14 @@
 
 const lang = localStorage.getItem("lang") || "es";
 document.documentElement.lang = lang;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-lang]").forEach(el => {
+    el.style.display = el.dataset.lang === lang ? "block" : "none";
+  });
+});
+
+function setLang(l){
+  localStorage.setItem("lang", l);
+  location.reload();
+}
